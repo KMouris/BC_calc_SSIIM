af53cfbe5156b33f767f7ffd62fe9375f201f72f
Maria Fernanda Morales Oreamuno <oreamuno@iws.uni-stuttgart.de>
2021-12-22 02:55:26 +0100

    read flow data, resample time to hourly

--- config.py
+++ config.py
@@ -22,8 +22,15 @@ except ModuleNotFoundError as e:
 """
 q_path: str, path where the .b16 WaSim data (detail input format)
 
-turbine_capacity: float, maximum turbine capacity. 
+turbine_capacity: float, maximum turbine capacity
+
+time_interval: int, with the following possible values: 
+    * 0 to keep original time interval
+    * 1 to resample original flow data to daily data
+    * 2 to resample original flow data to monthly data
 """
 q_path = r'Y:\Abt1\hiwi\Oreamuno\Tasks\01_BC_Calculation\Example_Files\WaSim\y_opt.b16'
 
 turbine_capacity = 108.02
+
+time_interval = 0
--- main_bc.py
+++ main_bc.py
@@ -9,3 +9,133 @@ https://github.com/KMouris/Sediment_Load_Calculation.
 from config import *
 
 
+# FUNCTIONS ----------------------------------------------------------------------------------------------------
+def extract_discharge(input_df):
+    """
+    Function reads the raw .b16 discharge output file, in data frame format, and extracts the discharges for the Devoll,
+    Holta, Zalli and Skebices sub-catchments. In the input file, columns with names {2, 3, 4, 5} (or indexes
+    {6, 7, 8, 9}) corresponds to the catchments {Zalli, Skebices, Holta, Devoll}. The output file needs the following
+    sub-catchment order: {Devoll, Holta, Zalli and Skebices}
+
+    Args:
+    ----------------------------------------------
+    :param input_df: data frame, with the original input data from the .b16 file
+
+    :return: np array, with the discharges for the [Devoll, Holta, Zalli and Skebices] sub catchments, in that order
+    """
+    output_q = np.full((input_df.shape[0], 4), 0.0)
+
+    output_q[:, 0] = input_df['5']   # Devoll
+    output_q[:, 1] = input_df['4']   # Holta
+    output_q[:, 2] = input_df['2']   # Zalli
+    output_q[:, 3] = input_df['3']   # Skebices
+
+    return output_q
+
+
+def calculate_outflows_constant_wl(inflow_array, turbine_capacity):
+    """
+    Function calculates the outflows based on a constant water level, meaning that inflows = outflows. The outflows are
+    through a turbine, with a maximum capacity, and a spillway, in case the inflow exceeds the turbine capacity. The
+    total inflow is calculated as the sum of the inflows from each sub-catchment (row-wise sum), and the outflow is
+    calculated for each time interval (each row).
+
+    Args:
+    --------------------------------------------
+    :param inflow_array: np.array, with the inflow data for each sub-catchment (in this case 4). Each column is a
+    different sub-catchment and each row corresponds to a different time interval.
+    :param turbine_capacity: float, turbine capacity
+
+    :return: np.array with [inflows (4), turbine outflow, spillway outflow], in that order.
+    """
+    total_inflow = np.sum(inflow_array, axis=1)
+
+    turbine_outflow = np.where(total_inflow > turbine_capacity, turbine_capacity, total_inflow)
+    spillway_outflow = np.where(total_inflow > turbine_capacity, total_inflow - turbine_capacity, 0)
+
+    # Total flow data
+    total_flow = np.c_[inflow_array, turbine_outflow, spillway_outflow]
+
+    return total_flow
+
+
+def modify_time_interval(df_time, flow_array, interval):
+    """
+    Function re-samples the initial time intervals to a new interval and averages the inflow data, for each watershed,
+    for the new time interval.
+
+    It first generates a date list, beginning from the initial time interval to the last, with the desired interval.
+    It then does 2 loops, the 1st one is through each new time interval and the 2nd through the original time data
+    frame. The original dates are read in order, and therefore the 2nd loop goes through all intervals within the new
+    time interval, and averages the flow data.
+
+    Args:
+    ----------------------------
+    :param df_time: data frame, with original time intervals, in datetime format
+    :param flow_array: np.array, with inflow data
+    :param interval: 1, if re-sample is to daily, 2 if re-sample is to monthly
+
+    :return: data frame, with new time intervals, and np.array, with the flow data averaged for the new time interval.
+
+    Note: Function currently only re-samples to daily and monthly data, and averages the flow data for the new interval.
+    If the user wants a different time interval or a different way to manage the flow data, they must do the
+    corresponding changes.
+    """
+
+    start_date = df_time[0]
+    end_date = df_time.iloc[-1]
+
+    if interval == 1:  # Daily (frequency is 'D')
+        resampled_dates = pd.to_datetime(pd.date_range(start_date, end_date, freq='D').strftime("%Y%m%d").tolist(),
+                                   format="%Y%m%d")
+        resampled_flow = np.full((len(resampled_dates), flow_array.shape[1]), 0.0)
+        initial = 0
+        for d, date in enumerate(resampled_dates):            # Loop through each day to consider
+            for i in range(initial, df_time.shape[0]):  # Loop through the original flow/date data
+                if date.day != df_time[i].day:
+                    resampled_flow[d, :] = np.mean(flow_array[initial:i, :], axis=0)
+
+                    initial = i
+                    break
+    elif interval == 2:  # monthly (frequency is "MS")
+        resampled_dates = pd.to_datetime(pd.date_range(start_date, end_date, freq='MS').strftime("%Y%m%d").tolist(),
+                                   format="%Y%m%d")
+        resampled_flow = np.full((len(resampled_dates), flow_array.shape[1]), 0.0)
+        initial = 0
+        for d, date in enumerate(resampled_dates):  # Loop through each day to consider
+            for i in range(initial, df_time.shape[0]):  # Loop through the original flow/date data
+                if date.month != df_time[i].month:
+                    resampled_flow[d, :] = np.mean(flow_array[initial:i, :], axis=0)
+
+                    initial = i
+                    break
+    else:
+        print(f"No resampling was done, since {interval} is not a valid entry")
+
+        return df_time, flow_array
+
+    return resampled_dates, resampled_flow
+
+
+# READ INPUT FILES ---------------------------------------------------------------------------------------------
+
+# read .b16 file with discharge data to df
+q_df = pd.read_csv(q_path, sep='\t', header=0, skiprows=[1, 2])
+# rename to convert to date time
+q_df.rename(columns={'YY': 'year', 'MM': 'month', 'DD': 'day', 'HH': 'hour'}, inplace=True)
+# Convert first 4 columns to datetime and save to array
+time_df = pd.to_datetime(q_df[['year', 'month', 'day', 'hour']])
+
+# Extract discharge data
+q_array = extract_discharge(q_df)
+
+# Option to convert to daily or monthly time discretization
+if time_interval != 0:
+    time_df, q_array = modify_time_interval(time_df, q_array, time_interval)
+
+# Calculate outflows and save inflows and outflows to array
+total_flows = calculate_outflows_constant_wl(q_array, turbine_capacity)
+
+# Calculate monthly volume (for concentration data)
+x=1
+
