485bd36dda54090b8004bd136acaf7eaacf22890
Harambula <47856971+Harambula@users.noreply.github.com>
2022-05-04 18:27:29 +0200

    Add files via upload
    Plot function now integrated in fun.py and config.py.
    Known Bug:
    Buffer overflow for large time spans on windows

--- config.py
+++ config.py
@@ -9,6 +9,9 @@ try:
     import glob
     import calendar
     import datetime
+    from dateutil import parser
+    import matplotlib.pyplot as mp
+    from matplotlib.dates import DateFormatter
 
 except ModuleNotFoundError as b:
     print('ModuleNotFoundError: Missing basic libraries (required: os, sys')
@@ -34,16 +37,24 @@ turbine_capacity: float, maximum turbine capacity
 time_interval: int, with the following possible values: 
     * 0 to keep original time interval
     * 1 to resample original flow data to daily data
-    * 2 to resample original flow data to monthly data
+    * 2 to resample original flow data to monthly data - Less accurate in seasonal water level calculations
     
-winter_threshold: list of int, with the months where the winter water level is applied
-summer_threshold: list of int, with the months where the summer water level is applied
+wl_threshold: list of int, with the target water levels for each month. This array reads only the first 12 values
+target_wl_upper_boundary: int, with the relative upper boundary to the target water level where the water level is kept 
+steady
+target_wl_lower_boundary: int, with the relative lower boundary to the target water level where the inflow is stored. 
+target_wl_maximum: int, with the maximum water level the reservoir can hold. If the water level rises above the excess 
+inflow is dumped
+If the water level is below target_wl_lower_boundary, turbine is turned off. Turbine is run at half of the inflow level,
+if the water level is between target_wl_lower_boundary and target water level from wl_threshold. When the level is in 
+between the target water level and target_wl_upper_boundary the turbine is kept at inflow level or max capacity if 
+exceeded. For water levels above the turbine is ran at full throttle. For water levels above the target_wl_maximum all 
+excess inflow is dumped to keep the water level at a maximum level of target_wl_maximum.
 
-upstream_wl_winter: float, with the constant upstream water level to be used in winter
-downstream_wl_winter: float, with the constant downstream water level to be used in winter
-upstream_wl_summer: float, with the constant upstream water level to be used in summer
-downstream_wl_summer: float, with the constant downstream water level to be used in summer
-    
+restrict_timei_date: boolean, if true the timei is trimed to the timeframe of timei_date_start and timei_date_end
+timei_date_start: str, with the start date for the timei file
+timei_date_end: str, with the end date for the timei file
+
 CONSTANT INPUT DATA: that must only be changed if more sub-catchments are to be added, or the order in the final files
  should be changed: 
 
@@ -58,23 +69,23 @@ sy_folder = os.path.abspath(r'../Input/Soil_Input')
 
 results_folder = os.path.abspath(r'../results')
 
-log_outflow_data = True
+plot_outflow_data = True
+plot_water_level = True
 
 turbine_capacity = 108.02
 
 time_interval = 0
 
-winter_threshold = [11, 12, 1, 2, 3, 4]
-summer_threshold = [5, 6, 7, 8, 9, 10]
+wl_threshold = [168,168,168,168,173,173,173,173,173,173,168,168]
+        # Boundaries are within target_volume ( + 0,1 wl)
+        # Emergency boundaries are within target_volume ( - 2 wl & 175 m asl)
+target_wl_upper_boundary = 0.1
+target_wl_lower_boundary = -2
+target_wl_maximum = 175
 
-upstream_wl_winter = 168
-downstream_wl_winter = 168
-upstream_wl_summer = 173
-downstream_wl_summer = 173
-
-
-#timei_date_start = datetime.datetime(2020, 1, 1)
-#timei_date_end = datetime.datetime(2025, 12, 31)
+restrict_timei_date = True
+timei_date_start = "01-01-2018"
+timei_date_end = "31-12-2018"
 
 catchment_order = ['Devoll', 'Holta', 'Zalli', 'Skebices']
 sediment_density = 2650
